f25092279d016291373bf1a116397412dba365d1
Akest <gizat.blt@gmail.com>
2024-06-12 08:00:19 +0300

    feat: locust, docs: README, style, refactor, chore

--- solution/app/postgres_init.py
+++ solution/app/postgres_init.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, Table, Column, Integer, String
-from sqlalchemy import MetaData, DateTime
+import sqlalchemy
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
 import datetime
@@ -14,47 +14,31 @@ class Items(Base):
     __tablename__ = 'items'
     id: Mapped[int] = mapped_column(primary_key=True)
     category_id: Mapped[int]
-    location_id: Mapped[int]
     title: Mapped[str]
-    description: Mapped[str]
     item_location: Mapped[str]
     starttime: Mapped[datetime.datetime] = mapped_column(
         default=datetime.datetime.now)
 
 
-def get_engine():
-    engine = create_engine(DB_ADDRESS, echo=True)
+def get_engine() -> sqlalchemy.engine.Engine:
+    engine = create_engine(DB_ADDRESS, echo=False)
     if not database_exists(engine.url):
         create_database(engine.url)
     return engine
 
 
-def create_items_table(engine):
+def create_items_table(engine: sqlalchemy.engine.Engine):
     metadata_obj = MetaData()
-    _ = Table(
-        'items',
-        metadata_obj,
-        Column('id', Integer, primary_key=True),
-        Column('category_id', Integer),
-        Column('location_id', Integer),
-        Column('title', String),
-        Column('description', String),
-        Column('item_location', String),
-        Column('starttime', DateTime, default=datetime.datetime.now)
-    )
-
     metadata_obj.create_all(engine)
 
 
-def insert_to_items(session_factory):
-    # TODO: Read real Data?
+def insert_to_items(session_factory: sessionmaker):
+    # TODO: Read real Data
     with session_factory() as session:
         for i in range(30):
             item = Items(
-                category_id=42,
-                location_id=7001,
+                category_id=60,
                 title=f'Title_{i}',
-                description=f'Description for {i}',
                 item_location=f'Address for {i}',
             )
             session.add(item)
--- solution/app/src/get_data.py
+++ solution/app/src/get_data.py
@@ -1,29 +1,48 @@
 from postgres_init import Items
 import pickle
+import pandas as pd
+
+from typing import List, Dict, Any
+import redis
+from flask_sqlalchemy import SQLAlchemy
 
 
-def set_history_to_user(redis_client, user_id, history):
+categories_mapping: Dict[int, str] = pd.read_csv(
+    'data/categories_mapping.csv',
+    dtype={'category_id': int},
+    index_col='category_id')\
+        .to_dict()['category_name']
+
+
+def get_category_by_id(category_id: int):
+    return categories_mapping[category_id]
+
+
+def set_history_to_user(redis_client: redis.Redis,
+                        user_id: int,
+                        history: List[Dict]):
     redis_client.set(user_id, pickle.dumps(history))
 
 
-def create_new_user(redis_client):
+def create_new_user(redis_client: redis.Redis) -> int:
     user_id = redis_client.dbsize() + 1
     set_history_to_user(redis_client, user_id, [])
     return user_id
 
 
-def get_item_info(db, item_id):
+def get_item_info(db: SQLAlchemy, item_id: int) -> Dict[str, Any]:
     item = db.get_or_404(Items, item_id,
                          description=f'Item с индексом {item_id} не найден.')
     return {
         'item_id': item.id,
         'title': item.title,
-        'description': item.description,
-        'start_date': item.starttime
+        'category': get_category_by_id(item.category_id),
+        'starttime': item.starttime.strftime("%d/%m/%Y, %H:%M:%S")
     }
 
 
-def get_user_history(user_id, redis_client):
+def get_user_history(user_id: int,
+                     redis_client: redis.Redis) -> List[Dict[str, Any]]:
     obj = redis_client.get(user_id)
     if obj is None:
         return None
@@ -34,10 +53,11 @@ def mock_recommendations():
     return list(range(1, 10))
 
 
-def get_model_predictions(history):
+def get_model_predictions(history: List[Dict[str, Any]]) -> List[int]:
     return mock_recommendations()
 
 
-def get_recommendations(history, db):
+def get_recommendations(db: SQLAlchemy,
+                        history: List[Dict]) -> List[Dict[str, Any]]:
     preds = get_model_predictions(history)
     return [get_item_info(db, item_id) for item_id in preds]
